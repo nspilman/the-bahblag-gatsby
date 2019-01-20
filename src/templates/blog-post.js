acfb11afae3c426827bc784809805c25d2d2304b
Nate Spilman <Nate.Spilman@gmail.com>
2019-01-20 14:40:25 -0800

    adding header

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,6 +3,7 @@ import { Link, graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
+import Header from '../components/Header'
 
 class BlogPostTemplate extends React.Component {
   render() {
@@ -14,33 +15,7 @@ class BlogPostTemplate extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title={post.frontmatter.title} description={post.excerpt} />
-       
-        <header id="header">
-						<h1><a href="/">Nate and Claire's Travel Blog</a></h1>
-						<nav className="links">
-							<ul>
-								<li><a href="#">Lorem</a></li>
-								<li><a href="#">Ipsum</a></li>
-								<li><a href="#">Feugiat</a></li>
-								<li><a href="#">Tempus</a></li>
-								<li><a href="#">Adipiscing</a></li>
-							</ul>
-						</nav>
-						<nav className="main">
-							<ul>
-								<li className="search">
-									<a className="fa-search" href="#search">Search</a>
-									<form id="search" method="get" action="#">
-										<input type="text" name="query" placeholder="Search" />
-									</form>
-								</li>
-								<li className="menu">
-									<a className="fa-bars" href="#menu">Menu</a>
-								</li>
-							</ul>
-						</nav>
-					</header>
-
+        <Header/>
 					<div id="main">
 
 							<article className="post">
