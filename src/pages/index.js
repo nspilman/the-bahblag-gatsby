dee2b03c304f3bbbf9194495af55ca9931d096a3
Nate Spilman <Nate.Spilman@gmail.com>
2019-01-20 14:49:57 -0800

    graphql update

--- src/pages/index.js
+++ src/pages/index.js
@@ -85,7 +85,7 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC filter: {fileAbsolutePath: {regex: "/(blog)/.*\\.md$/"}}}) {
       edges {
         node {
           excerpt
