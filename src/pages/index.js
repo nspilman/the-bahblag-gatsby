d2c1052a4d9dfa9ddb273867df26d01c72bada04
Nate Spilman <Nate.Spilman@gmail.com>
2019-01-21 12:09:37 -0800

    adding main images to posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,7 +39,7 @@ class BlogIndex extends React.Component {
     </header>
     <a href="#" className="image featured">
     {console.log(node.frontmatter.image)}
-        <Img sizes={node.frontmatter.image.childImageSharp.fluid.sizes} />
+        <Img sizes={node.frontmatter.image.childImageSharp.sizes} />
     </a>
     <p dangerouslySetInnerHTML={{ __html: node.excerpt }}></p>
     <footer>
