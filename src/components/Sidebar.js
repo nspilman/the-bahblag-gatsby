498a08e26ba404735cc344519e70e68597d84a5e
Nate Spilman <Nate.Spilman@gmail.com>
2019-01-22 17:57:18 -0800

    updating sidebar - removing stock stuff

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -112,7 +112,8 @@ render(){
 
 							<section className="blurb">
 								<h2>About</h2>
-								<p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod amet placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at phasellus sed ultricies.</p>
+								<p>
+This is Nate and Claire's travel blog!</p>
 								<ul className="actions">
 									<li><a href="#" className="button">Learn More</a></li>
 								</ul>
