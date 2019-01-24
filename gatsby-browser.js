deb632f8110f6218c36b08142a02c2e2d54c5274
Nate Spilman <Nate.Spilman@gmail.com>
2019-01-23 21:52:14 -0800

    adding Bootstrap

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,5 +3,5 @@ import 'typeface-montserrat'
 import 'typeface-merriweather'
 
 import "./src/styles/main.css"
-import "./src/styles/bootstrap.css"
-import "./src/styles/bootstrap-theme.css"
+import '~bootstrap/dist/css/bootstrap.css';
+
