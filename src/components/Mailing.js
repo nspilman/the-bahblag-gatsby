12a73f6c89af180bbcc9fd34177a48c65245aed2
Nate Spilman <Nate.Spilman@gmail.com>
2019-01-28 16:37:32 +0000

    updating form to say thanks for submission

--- src/components/Mailing.js
+++ src/components/Mailing.js
@@ -25,60 +25,48 @@ this.updateEmail = this.updateEmail.bind(this);
                     'content-type': 'application/json',
             }}
         ).then(resp =>
+            {if(resp.status == "200"){
+                localStorage.setItem('mailSub',true)
+            }
             console.log(resp)
+        }
         )
     }
 
-    addEmail2(){
-        console.log(this.state.email)
-        fetch(
-            "https://artsonthehudson.com/921gsheet/blog",
-            {
-                method:"POST",
-            mode: "no-cors", // no-cors, cors, *same-origin
-        headers: {
-            "Content-Type": "application/json",
-            // "Content-Type": "application/x-www-form-urlencoded",
-        },
-        redirect: "follow", // manual, *follow, error
-        referrer: "no-referrer", // no-referrer, *client
-        body: JSON.stringify({
-            "email":this.state.email
-        }), // body data type must match "Content-Type" header
-    })
-    .then(response => response.json()); // parses response to JSON
-}
-addEmail3(){
-    axios.post(
-        'https://artsonthehudson.com/921gsheet',{
-            "group":"9999",
-            "firstname":"testUser",
-            "function":"login"
-            }
-            ).then(response => console.log(response))
-.catch(e => {
-  this.errors.push(e)
-})
-}
+    returnForm(){
+        return(
+            <div>
+        <form>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Join our Mailing List</label>
+            <input type="email" onChange={this.updateEmail} value={this.state.email} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+          </div>
+          <button type="button" class="btn btn-primary" style={{height:'3em'}} onClick={this.addEmail} value="Submit">submit</button>
+          </form>
         
+        </div>
+        )
+    }
+        
+    returnThanks(){
+        return(
+            <div>
+                Thank you for signing up!
+            </div>
+        )
+    }
 
     updateEmail(e){
         this.setState({email: e.target.value})
     }   
 
     render() {
-return(
-    <div>
-<form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Join our Mailing List</label>
-    <input type="email" onChange={this.updateEmail} value={this.state.email} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-  </div>
-  <button type="button" class="btn btn-primary" style={{height:'3em'}} onClick={this.addEmail} value="Submit">submit</button>
-  </form>
-
-</div>
-
-)}}
+        if(localStorage.getItem('mailSub')){
+            this.returnThanks()
+        }
+        else{
+            this.returnForm()
+        }
+}}
 
 export default Mailing
