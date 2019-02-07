650a3ac103d24f7ae3b18ea0d16971f647bc91d3
Nate Spilman <Nate.Spilman@gmail.com>
2019-02-07 08:13:44 +0200

    adding comment box

--- src/components/BlogCommentBox.js
+++ src/components/BlogCommentBox.js
@@ -3,12 +3,26 @@ import React from 'react'
 
 
 class BlogCommentBox extends React.Component {
+     constructor(){
+            super();
+            this.state = {
+               email: null,
+               author:null,
+               comment:null,
+               status:null,
+            }
+            this.addEmail = this.addEmail.bind(this);
+    this.updateEmail = this.updateEmail.bind(this);
+        }
 render(){
     return(
         
 <div className="form-group">
+
 <label for="usr">Name:</label>
-  <input type="text" class="form-control" id="usr"></input>
+  <input type="text" className="form-control" id="name"></input>
+<label for="usr">Email:</label>
+  <input type="text" className="form-control" id="email"></input>
   <label for="comment">Comment:</label>
   <textarea className="form-control" rows="5" id="comment"></textarea>
 </div>
