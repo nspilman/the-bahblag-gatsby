271ab2c18363ec7ff9dbe90e2a033b6a1fbf7c6a
Nate Spilman <Nate.Spilman@gmail.com>
2019-03-06 09:13:11 +0000

    fixing images

--- src/components/indexMain.js
+++ src/components/indexMain.js
@@ -9,7 +9,7 @@ render(){
     return(
     <div id="main">
     {posts.map(({ node }) => {
-      console.log(node)
+      console.log(node.frontmatter)
       return (
 
 <article className="post">
